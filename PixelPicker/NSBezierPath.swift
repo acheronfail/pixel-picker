1305e4366a0153e38ee904ff95840effda7e468b
acheronfail <acheronfail@gmail.com>
2018-05-30 15:20:18 +1000

    chore: refactoring code; move NSBezierPath extension to its own file

---
+++ PixelPicker/NSBezierPath.swift
@@ -0,0 +1,22 @@
+//
+//  NSBezierPath.swift
+//  PixelPicker
+//
+
+// Extend NSBezierPath to allow easy conversion to a CGPath.
+// https://developer.apple.com/library/content/documentation/Cocoa/Conceptual/CocoaDrawingGuide/Paths/Paths.html#//apple_ref/doc/uid/TP40003290-CH206-SW2
+extension NSBezierPath {
+    var cgPath: CGPath {
+        let path = CGMutablePath()
+        var points = [CGPoint](repeating: .zero, count: 3)
+        for i in 0 ..< self.elementCount {
+            switch self.element(at: i, associatedPoints: &points) {
+            case .moveToBezierPathElement: path.move(to: points[0])
+            case .lineToBezierPathElement: path.addLine(to: points[0])
+            case .curveToBezierPathElement: path.addCurve(to: points[2], control1: points[0], control2: points[1])
+            case .closePathBezierPathElement: path.closeSubpath()
+            }
+        }
+        return path
+    }
+}
